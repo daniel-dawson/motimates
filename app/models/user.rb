6bfa0909937345b56a23b85b619389f965fba09e
Daniel Dawson <dannyldawson@gmail.com>
2019-08-22 12:58:48 -0700

    add model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :preference
+  has_many :connections
+  has_many :memberships
+  has_many :communities, through: :memberships
+  has_many :goals, through: :communities
+  has_many :connected_users, through: :connections
 end
