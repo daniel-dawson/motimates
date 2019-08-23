5ddcfbb07fd30f1cd807bb2a5308285deacba6b7
Daniel Dawson <dannyldawson@gmail.com>
2019-08-23 11:46:53 -0700

    add user_id to goals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_160715) do
+ActiveRecord::Schema.define(version: 2019_08_23_184534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_08_22_160715) do
     t.integer "community_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "memberships", force: :cascade do |t|
