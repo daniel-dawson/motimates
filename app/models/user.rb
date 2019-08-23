79c8c7aade103fef9b103adb88ce251db6b55379
Daniel Dawson <dannyldawson@gmail.com>
2019-08-23 13:35:44 -0700

    commented out failed experiments Attempted to simplify bidirectional associations with built-in methods... it failed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :preference
+
   has_many :connections
+  has_many :connected_users, through: :connections, dependent: :destroy
+  # has_many :inverse_connections, class_name: 'Connection', foreign_key: 'connected_user_id', inverse_of: :user
+  # has_many :inverse_connected_users, through: :inverse_connections, source: :user, dependent: :destroy
+
   has_many :memberships
   has_many :communities, through: :memberships
   has_many :goals, through: :communities
-  has_many :connected_users, through: :connections
+
 end
