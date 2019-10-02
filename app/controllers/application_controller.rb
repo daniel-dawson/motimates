4fe077f64e0f565aece43772265194aade192076
Daniel Dawson <dannyldawson@gmail.com>
2019-10-01 23:27:45 -0700

    redirect correctly regardless of login status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do | exception |
-    redirect_to root_url, alert: exception.message
+    redirect_to user_path(current_user), alert: exception.message if user_signed_in?
+    redirect_to root_path, alert: exception.message if !user_signed_in?
   end
 end
