edd2d1e3b2f1c6b368d7de0ccea9d8d10dabb4e4
Daniel Dawson <dannyldawson@gmail.com>
2019-10-02 18:05:35 -0700

    eliminate all wrapping due to fields_with_errors proc

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Motimates
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.action_view.field_error_proc = Proc.new { |html_tag, instance|
-      "<div class=\"ui error message\">#{html_tag}</div>".html_safe
+      html_tag
     }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
