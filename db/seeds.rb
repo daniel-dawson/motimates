ab33484a73c8339619b6b71be422e2dfdbd4477b
Daniel Dawson <dannyldawson@gmail.com>
2019-10-09 10:51:11 -0700

    add community seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Community.create([
+  { name: "Diet",
+    description: "Work on your diet goals, including gaining weight, cutting back on certain items,
+                  and food sourcing." },
+  { name: "Exercise",
+    description: "Goals might include going to classes x times per week, incorporating running into your routine
+                  , etc." },
+  { name: "Career",
+    description: "Network, discover new career paths, quit your current job, etc." },
+  { name: "Finance",
+    description: "Pay down debt, work on budgeting, invest." },
+  { name: "Social",
+    description: "Go to more concerts, dance with friends on the weekend, host board game nights." },
+  { name: "Hobby",
+    description: "Dedicate more time towards a passion, pick up a new hobby, recruit others." },
+  { name: "Education",
+    description: "Download a relevant learning application, do x lessons per day, go to a meetup, study x hours per week." }
+  ])
