6d28a9c79aad0db54834c0923bb4154b91f8b575
Daniel Dawson <dannyldawson@gmail.com>
2019-10-10 11:53:36 -0700

    wire goals controller
    Broke up cancan actions because association is nested, so cannot be retrieved by macros.
    
    TODO: Rearrange associations so goals are wired directly to user

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,24 @@
 class GoalsController < ApplicationController
-  load_and_authorize_resource through: :current_user, shallow: true
+  load_resource :user
+  load_and_authorize_resource
   def index
+  end
+
+  def new
+
+  end
+
+  def create
+    if @goal.save
+      @user.goals << @goal
+      flash[:message] = "Goal created"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def goal_params
+    params.require(:goal).permit(:name, :description, :community_id)
   end
 end
