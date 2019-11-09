6f1a6e7ebae25791ee525e73e67286ecf5770d59
Daniel Dawson <dannyldawson@gmail.com>
2019-11-09 08:50:18 -0800

    add helper to generate avatar image tag
    user: user you want image for
    size: desired image size in pixels
    class_string: sets class attributes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def flash_class_name(name)
     name == "alert" ? "ui error message" : "ui info message"
   end
+
+  def avatar_image_tag(user, size, class_string)
+    if user.avatar.attached?
+      if user.avatar.variable?
+        image_tag(user.avatar.variant(resize: "#{size}x#{size}!"), class: "#{class_string}")
+      else
+        image_tag(gravatar_image_url(user.email, size: size), height: size, width: size, class: "#{class_string}")
+      end
+    else
+      image_tag(gravatar_image_url(user.email, size: size), height: size, width: size, class: "#{class_string}")
+    end
+  end
 end
