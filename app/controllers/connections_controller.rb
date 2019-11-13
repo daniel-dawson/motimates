cfdc78d627a8dab489735ca82d26ff681967572e
Daniel Dawson <dannyldawson@gmail.com>
2019-11-12 16:04:04 -0800

    allow for some actions to respond to json formats
    Previously only responded to html.

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,18 +1,36 @@
 class ConnectionsController < ApplicationController
-  before_action :authenticate_user!
-  load_and_authorize_resource :user
-  before_action :set_motimate
+  before_action :authenticate_user!, except: [:show]
+  load_and_authorize_resource :user, except: [:show]
+  before_action :set_motimate, except: [:show]
+  # load_resource :motimate
   # load_resource :motimate, through: :user, only: [:create, :accept]
 
   def index
   end
 
-  def create
-    Connection.request(@user, @motimate)
-    flash[:success] = "Motimate request sent"
-    redirect_to user_path(@user)
+  def show
+    c = Connection.find_by_id(params[:id])
+    respond_to do |format|
+      format.json { render json: c }
+    end
   end
 
+  def create
+    params[:note].strip.empty? ?
+      Connection.request(@user, @motimate) :
+      Connection.request(@user, @motimate, params[:note])
+    if Connection.connected?(@user, @motimate)
+      flash[:success] = "Motimate request sent"
+      connection = Connection.find_by(user_id: @user.id, motimate_id: @motimate.id)
+      respond_to do |format|
+        format.html { redirect_to user_path(@user) }
+        format.json { render json: connection }
+      end
+    else
+      flash[:notice] = "Users weren't connected"
+      redirect_to user_path(@user)
+    end
+  end
 
   def accept
     if @user.motimate_requests.include?(@motimate)
@@ -21,10 +39,18 @@ class ConnectionsController < ApplicationController
     else
       flash[:notice] = "No friendship request from #{@motimate.name.humanize}."
     end
-    redirect_to user_path(@user)
+    connection = Connection.find_by(user_id: @user.id, motimate_id: @motimate.id)
+    respond_to do |format|
+      format.html
+      format.json { render json: connection }
+    end
   end
 
   private
+
+  def serializer
+    ConnectionSerializer
+  end
 
   def set_motimate
     @motimate = User.find_by_id(params[:motimate_id])
