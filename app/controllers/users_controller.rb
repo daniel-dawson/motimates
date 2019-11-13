e00160a8e41b449affb3dd2911e9eb1cc8d4019b
Daniel Dawson <dannyldawson@gmail.com>
2019-11-12 16:04:48 -0800

    add method to remove a user's avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,18 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
 
-  def index 
+  def index
   end
 
   def show
-
   end
+
+  def delete_avatar
+    @user.avatar.purge
+    flash[:message] = "Uploaded picture deleted"
+    redirect_to edit_user_registration_path
+  end
+
+  private
+
 end
