d5176b5497c6f8d45f9ef8241deb9becc7ffd882
Daniel Dawson <dannyldawson@gmail.com>
2019-11-18 19:51:28 -0800

    set up serialization for motimates
    Could not find a way to access url_helpers by extending functionality.
    Calling it directly looks ugly but seems to be the best/only way.

---
+++ app/serializers/motimate_serializer.rb
@@ -0,0 +1,14 @@
+class MotimateSerializer
+  include FastJsonapi::ObjectSerializer
+  include Rails.application.routes.url_helpers
+  extend AvatarHelper
+  set_key_transform :camel_lower
+
+  attributes :name, :age, :gender, :location
+  attribute :avatar_url do |moti|
+    Rails.application.routes.url_helpers.rails_blob_url(moti.avatar) if moti.avatar.attached?
+  end
+  attribute :default_avatar_url do |moti|
+    gravatar_url_for moti
+  end
+end
