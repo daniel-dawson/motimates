7782879fe24abc320efcc9df6a7b5b2e64567902
Daniel Dawson <dannyldawson@gmail.com>
2019-11-18 16:35:30 -0800

    remove render method override
    This was a cute idea to make controllers DRYer, but introduced
    challenges when custom options needed.
    
    Would like to implement this at some point, but shelfing it for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,4 @@ class ApplicationController < ActionController::Base
     redirect_to user_path(current_user), alert: exception.message if user_signed_in?
     redirect_to root_path, alert: exception.message if !user_signed_in?
   end
-
-  def render(options = {})
-    options[:json] = serializer.new(options[:json]) if serializer
-    super(options)
-  end
-
-  def serializer
-  end
 end
