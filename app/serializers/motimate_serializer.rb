a5017b7e7f77c706b9bf0d6b9714d44fb27b5a52
Daniel Dawson <dannyldawson@gmail.com>
2019-11-20 17:10:11 -0800

    use params to build accepted_at attribute
    Not sure I like the name of the attribute, but it will do for now.

--- app/serializers/motimate_serializer.rb
+++ app/serializers/motimate_serializer.rb
@@ -1,9 +1,15 @@
+require "pry"
+
 class MotimateSerializer
   include FastJsonapi::ObjectSerializer
   include Rails.application.routes.url_helpers
   extend AvatarHelper
   set_key_transform :camel_lower
 
+  attribute :accepted_at do |moti, params|
+    moti.connections.where(motimate_id: params[:user_id], user_id: moti.id)[0].accepted_at if params[:user_id].present?
+  end
+  
   attributes :name, :age, :gender, :location
   attribute :avatar_url do |moti|
     Rails.application.routes.url_helpers.rails_blob_url(moti.avatar) if moti.avatar.attached?
